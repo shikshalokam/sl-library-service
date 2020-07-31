dc084a2a2b8c67d95fdf4637c5391eb1f3f60d81
Akash Shah <aks301190@gmail.com>
2020-07-31 19:15:48 +0530

    Format changes.

--- generics/constants/api-responses.js
+++ generics/constants/api-responses.js
@@ -8,8 +8,8 @@
 
 module.exports = {
     SUNBIRD_SERVICE_DOWN: "Sunbird service is down",
-    FILTERS_FOUND:"Filters fetched successfully",
-    FILTERS_NOT_FOUND:"Filters not found",
+    FILTERS_FOUND: "Filters fetched successfully",
+    FILTERS_NOT_FOUND: "Filters not found",
     LEARNING_RESORCES_FOUND: "Learning resources found successfully",
     LEARNING_RESORCES_NOT_FOUND: "Learning resources not found"
 };
